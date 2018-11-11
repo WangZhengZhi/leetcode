9b1f92edf103eeace826d56061f6ffc38c3c01ec
zhengzhistudio <zhengzhistudio@gmail.com>
2018-11-11 15:57:41 +0800

    Update 二分查找/练习测试/非递归版本/main.go

--- 二分查找/练习测试/非递归版本/main.go
+++ 二分查找/练习测试/非递归版本/main.go
@@ -1,20 +1,29 @@
 package main
 
-/*非递归版本的二分查找*/
-func main() {
+import (
+	"fmt"
+)
 
-}
+/*非递归版本的二分查找*/
+
 func BinarySearch(arr []int, low, high, target int) int {
 	mid := low + (high-low)/2
-	if arr[mid] == target {
-		return mid
+	for low <= high {
+		if arr[mid] == target {
+			return mid
 
-	} else if arr[mid] > target {
-		high = mid
+		} else if arr[mid] > target {
+			high = mid
 
-	} else if arr[mid] < target {
-		low = mid
+		} else if arr[mid] < target {
+			low = mid
 
+		}
 	}
-	return 0
+
+	return -1
+}
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println(BinarySearch(arr,0,7,4))
 }
